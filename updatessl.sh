c71a130b511d5f8e81ef88f54113e14d43879754
cjsund <cjsund@126.com>
2014-04-09 14:42:38 +0800

    add updatessl.sh

---
+++ updatessl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+openssl version
+if [[ `openssl version` == 'OpenSSL 1.0.1g 7 Apr 2014' ]]
+then
+    echo "openssl is already new version"
+    exit 2
+fi
+wget http://www.openssl.org/source/openssl-1.0.1g.tar.gz
+tar -xzvf openssl-1.0.1g.tar.gz
+cd openssl-1.0.1g/
+yum -y install zlib
+./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared
+make && make install
+mv /usr/bin/openssl /usr/bin/openssl.old
+mv /usr/include/openssl /usr/include/openssl.old
+mv /usr/lib/libssl.so /usr/lib/libssl.so.bak
+ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+ln -s /usr/local/ssl/include/openssl /usr/include/openssl
+ln -s /usr/local/ssl/lib/libssl.so.1.0.0 /usr/lib/libssl.so
+echo "/usr/local/ssl/lib" >> /etc/ld.so.conf
+/sbin/ldconfig -v
+/usr/bin/openssl version
