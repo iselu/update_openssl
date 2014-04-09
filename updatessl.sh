b95b0ac1775f4269d0cb6c5e59846e273563b781
cjsund <cjsund@126.com>
2014-04-09 15:18:24 +0800

    基础更新

--- updatessl.sh
+++ updatessl.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
-openssl version
+
 if [[ `openssl version` == 'OpenSSL 1.0.1g 7 Apr 2014' ]]
 then
-    echo "openssl is already new version"
+    echo "openssl is OpenSSL 1.0.1g 7 Apr 2014"
     exit 2
 fi
+echo "You openssl version"
+openssl version
 wget http://www.openssl.org/source/openssl-1.0.1g.tar.gz
 tar -xzf openssl-1.0.1g.tar.gz
 cd openssl-1.0.1g/
@@ -20,4 +22,5 @@ ln -s /usr/local/ssl/include/openssl /usr/include/openssl
 ln -s /usr/local/ssl/lib/libssl.so.1.0.0 /usr/lib/libssl.so
 echo "/usr/local/ssl/lib" >> /etc/ld.so.conf
 /sbin/ldconfig -v
+echo "Now openssl version"
 /usr/bin/openssl version
